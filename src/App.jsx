26e047a75bac45cdd9b07cc32eec3ca9331ada52
Lovish Duggal <lovishduggal11@gmail.com>
2024-07-22 08:38:20 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <Button className="p-2">Subscribe to newsletter</Button>
+    </>
+  );
+}
+
+export default App;
