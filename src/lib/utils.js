d5e0adaffeadb2ad653a71b2b103c27bb47d593a
Lovish Duggal <lovishduggal11@gmail.com>
2024-07-25 11:55:33 +0530

    add AI summary generation and copy to clipboard functionality

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const getPrompt = (title) => {
+  return `Job Title: ${title} , Depends on job title give me list of  summery for 3 experience level, Mid Level and Freasher level in 3 -4 lines in array format, With summery and experience_level Field in JSON Format`;
+};
--- src/pages/dashboard/resume/components/forms/Summary.jsx
+++ src/pages/dashboard/resume/components/forms/Summary.jsx
@@ -1,4 +1,4 @@
-import { BrainCircuit, Loader2 } from 'lucide-react';
+import { BrainCircuit, Check, ClipboardCopy, Loader2 } from 'lucide-react';
 import { Button } from '../../../../../components/ui/button';
 import { Textarea } from '../../../../../components/ui/textarea';
 import ResumeDataContext from '../../../../../context/ResumeDataContext';
@@ -6,16 +6,39 @@ import { useContext, useState } from 'react';
 import { toast } from 'sonner';
 import { updateResume } from '../../../../../http/api';
 import { useParams } from 'react-router-dom';
+import { aiChatSession } from '../../../../../services/generativeAi';
+import { getPrompt } from '../../../../../lib/utils';
 
+//TODO:
+//? Optimization and refactoring is pending!!
 const Summary = ({ setEnableNxtBtn }) => {
   const { resumeData, setResumeData } = useContext(ResumeDataContext);
   const { resumeId } = useParams();
   const [summary, setSummary] = useState('');
+  const [generatedSummaryList, setGeneratedSummaryList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [loadingGenerate, setLoadingGenerate] = useState(false);
+  const [copiedIdx, setCopiedIdx] = useState(null);
 
   const handleInputChange = (e) => {
     setSummary(e.target.value);
     setResumeData((prev) => ({ ...prev, summary: e.target.value }));
+  };
+
+  const handleCopyToClipboard = (summary, idx) => {
+    navigator.clipboard.writeText(summary);
+    setCopiedIdx(idx);
+    setTimeout(() => {
+      setCopiedIdx(null);
+    }, 2000);
+  };
+
+  const handleGenerate = async () => {
+    setLoadingGenerate(true);
+    const prompt = getPrompt(resumeData?.jobTitle);
+    const result = await aiChatSession.sendMessage(prompt);
+    setGeneratedSummaryList(JSON.parse(result.response.text()));
+    setLoadingGenerate(false);
   };
 
   const handleSubmit = async (e) => {
@@ -41,33 +64,61 @@ const Summary = ({ setEnableNxtBtn }) => {
   return (
     <div className="shadow-lg p-2 rounded-lg border-t-primary border-t-4">
       <h2 className="font-bold text-base">Summary</h2>
-      <p className="text-sm font-medium">
-        Brief summary of your experience and skills
-      </p>
+      <p className="text-sm font-medium">Brief Summary</p>
       <form onSubmit={handleSubmit} className="mt-7">
         <div className="flex flex-wrap justify-between items-center">
           <label className="font-medium">Add Summary</label>
           <Button
+            type="button"
             className="fex  items-center gap-2 border-primary"
             variant="outline"
             size="sm"
+            onClick={handleGenerate}
           >
-            <BrainCircuit size={'20px'} /> Generate with AI
+            <BrainCircuit
+              size={'16px'}
+              className={`${
+                loadingGenerate ? ' animate-pulse' : 'animate-none'
+              }`}
+            />{' '}
+            Generate with AI
           </Button>
         </div>
         <Textarea
+          rows={10}
           className="mt-4"
           onChange={handleInputChange}
           value={summary}
           required
         />
-        <div className="flex justify-end  mt-2">
+        <div className="flex justify-end  mt-4">
           <Button disabled={loading} type="submit" className="flex gap-1">
             {loading && <Loader2 className="animate-spin" />}
             Save
           </Button>
         </div>
       </form>
+
+      {generatedSummaryList.length > 0 && (
+        <div className="mt-5">
+          <h2 className="font-bold text-lg">Generated Suggestions</h2>
+          {generatedSummaryList.map((item, idx) => (
+            <div key={idx} className="mt-2 border rounded-lg px-3 py-2">
+              <h2 className="mb-1 text-sm font-medium flex items-center justify-between">
+                Experience Level: {item.experience_level}{' '}
+                <span onClick={() => handleCopyToClipboard(item.summary, idx)}>
+                  {copiedIdx === idx ? (
+                    <Check size={'14px'} />
+                  ) : (
+                    <ClipboardCopy className="cursor-pointer" size={'14px'} />
+                  )}
+                </span>
+              </h2>
+              <p className="text-sm text-pretty">{item.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
