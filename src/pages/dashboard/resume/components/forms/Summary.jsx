33fca9daa26ce6fefe42430cbbdb0f5929636762
Lovish Duggal <lovishduggal11@gmail.com>
2024-07-26 09:20:22 +0530

    optimization and refactoring of summary section

--- src/pages/dashboard/resume/components/forms/Summary.jsx
+++ src/pages/dashboard/resume/components/forms/Summary.jsx
@@ -2,23 +2,25 @@ import { BrainCircuit, Check, ClipboardCopy, Loader2 } from 'lucide-react';
 import { Button } from '../../../../../components/ui/button';
 import { Textarea } from '../../../../../components/ui/textarea';
 import ResumeDataContext from '../../../../../context/ResumeDataContext';
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import { updateResume } from '../../../../../http/api';
 import { useParams } from 'react-router-dom';
 import { aiChatSession } from '../../../../../services/generativeAi';
 import { getPrompt } from '../../../../../lib/utils';
 
-//TODO:
-//? Optimization and refactoring is pending!!
+
 const Summary = ({ setEnableNxtBtn }) => {
   const { resumeData, setResumeData } = useContext(ResumeDataContext);
   const { resumeId } = useParams();
   const [summary, setSummary] = useState('');
   const [generatedSummaryList, setGeneratedSummaryList] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [loadingGenerate, setLoadingGenerate] = useState(false);
+  const [isLoading, setIsLoading] = useState({
+    submit: false,
+    generate: false,
+  });
   const [copiedIdx, setCopiedIdx] = useState(null);
+  const timeoutRef = useRef(null);
 
   const handleInputChange = (e) => {
     setSummary(e.target.value);
@@ -28,22 +30,32 @@ const Summary = ({ setEnableNxtBtn }) => {
   const handleCopyToClipboard = (summary, idx) => {
     navigator.clipboard.writeText(summary);
     setCopiedIdx(idx);
-    setTimeout(() => {
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
       setCopiedIdx(null);
     }, 2000);
   };
 
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
   const handleGenerate = async () => {
-    setLoadingGenerate(true);
+    setIsLoading((prev) => ({ ...prev, generate: true }));
     const prompt = getPrompt(resumeData?.jobTitle);
     const result = await aiChatSession.sendMessage(prompt);
     setGeneratedSummaryList(JSON.parse(result.response.text()));
-    setLoadingGenerate(false);
+    setIsLoading((prev) => ({ ...prev, generate: false }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    setIsLoading((prev) => ({ ...prev, submit: true }));
 
     try {
       const updatedResumeData = {
@@ -57,10 +69,9 @@ const Summary = ({ setEnableNxtBtn }) => {
       console.error('Error updating resume:', error);
       toast('Error updating resume. Please try again.', { type: 'error' });
     } finally {
-      setLoading(false);
+      setIsLoading((prev) => ({ ...prev, submit: false }));
     }
   };
-
   return (
     <div className="shadow-lg p-2 rounded-lg border-t-primary border-t-4">
       <h2 className="font-bold text-base">Summary</h2>
@@ -70,17 +81,16 @@ const Summary = ({ setEnableNxtBtn }) => {
           <label className="font-medium">Add Summary</label>
           <Button
             type="button"
-            className="fex  items-center gap-2 border-primary"
+            className="flex items-center gap-2 border-primary"
             variant="outline"
             size="sm"
             onClick={handleGenerate}
+            disabled={isLoading.generate}
           >
             <BrainCircuit
-              size={'16px'}
-              className={`${
-                loadingGenerate ? ' animate-pulse' : 'animate-none'
-              }`}
-            />{' '}
+              size={16}
+              className={isLoading.generate ? 'animate-pulse' : 'animate-none'}
+            />
             Generate with AI
           </Button>
         </div>
@@ -91,9 +101,13 @@ const Summary = ({ setEnableNxtBtn }) => {
           value={summary}
           required
         />
-        <div className="flex justify-end  mt-4">
-          <Button disabled={loading} type="submit" className="flex gap-1">
-            {loading && <Loader2 className="animate-spin" />}
+        <div className="flex justify-end mt-4">
+          <Button
+            disabled={isLoading.submit}
+            type="submit"
+            className="flex gap-1"
+          >
+            {isLoading.submit && <Loader2 className="animate-spin" />}
             Save
           </Button>
         </div>
@@ -105,12 +119,12 @@ const Summary = ({ setEnableNxtBtn }) => {
           {generatedSummaryList.map((item, idx) => (
             <div key={idx} className="mt-2 border rounded-lg px-3 py-2">
               <h2 className="mb-1 text-sm font-medium flex items-center justify-between">
-                Experience Level: {item.experience_level}{' '}
+                Experience Level: {item.experience_level}
                 <span onClick={() => handleCopyToClipboard(item.summary, idx)}>
                   {copiedIdx === idx ? (
-                    <Check size={'14px'} />
+                    <Check size={14} />
                   ) : (
-                    <ClipboardCopy className="cursor-pointer" size={'14px'} />
+                    <ClipboardCopy className="cursor-pointer" size={14} />
                   )}
                 </span>
               </h2>
